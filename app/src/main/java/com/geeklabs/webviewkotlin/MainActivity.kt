8305faa3b203f047904316a0e8f20e422b11d3e1
Shylendra Madda <shylendra.madda@tylertech.com>
2020-07-23 14:29:26 +0530

    Initial commit

---
+++ app/src/main/java/com/geeklabs/webviewkotlin/MainActivity.kt
@@ -0,0 +1,148 @@
+package com.geeklabs.webviewkotlin
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.KeyEvent
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var progress: Progress? = null
+    private var isLoaded: Boolean = false
+    private var doubleBackToExitPressedOnce = false
+    private var webURL = "https://www.geeklabs.co.in/" // Change it with your URL
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView.settings.javaScriptEnabled = true
+        if (!isOnline()) {
+            showToast(getString(R.string.no_internet))
+            infoTV.text = getString(R.string.no_internet)
+            showNoNetSnackBar()
+            return
+        }
+    }
+
+    override fun onResume() {
+        if (isOnline() && !isLoaded) loadWebView()
+        super.onResume()
+    }
+
+    private fun loadWebView() {
+        infoTV.text = ""
+        webView.loadUrl(webURL)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val url = request?.url.toString()
+                view?.loadUrl(url)
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                setProgressDialogVisibility(true)
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                isLoaded = true
+                setProgressDialogVisibility(false)
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+                val errorMessage = "Got Error! $error"
+                showToast(errorMessage)
+                infoTV.text = errorMessage
+                setProgressDialogVisibility(false)
+                super.onReceivedError(view, request, error)
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    showToastToExit()
+                }
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun showToastToExit() {
+        when {
+            doubleBackToExitPressedOnce -> {
+                onBackPressed()
+            }
+            else -> {
+                doubleBackToExitPressedOnce = true
+                showToast(getString(R.string.back_again_to_exit))
+                Handler(Looper.myLooper()!!).postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+            }
+        }
+    }
+
+    private fun setProgressDialogVisibility(visible: Boolean) {
+        if (visible) progress = Progress(this, R.string.please_wait, cancelable = true)
+        progress?.apply { if (visible) show() else dismiss() }
+    }
+
+    private fun isOnline(): Boolean {
+        val connectivityManager =
+                getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showNoNetSnackBar() {
+        val snack = Snackbar.make(rootView, getString(R.string.no_internet), Snackbar.LENGTH_INDEFINITE)
+        snack.setAction(getString(R.string.settings)) {
+            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        }
+        snack.show()
+    }
+}
---
+++ app/src/main/java/com/geeklabs/webviewkotlin/Progress.kt
@@ -0,0 +1,47 @@
+package com.geeklabs.webviewkotlin
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.StringRes
+
+class Progress constructor(
+    context: Context?,
+    @StringRes private val titleRes: Int,
+    cancelable: Boolean = false
+) {
+
+    private var view: View? = null
+    private var builder: AlertDialog.Builder
+    private var dialog: Dialog
+
+    init {
+        view = LayoutInflater.from(context).inflate(R.layout.progress, null)
+        view?.findViewById<TextView>(R.id.text)?.setText(titleRes)
+        builder = AlertDialog.Builder(context)
+        builder.setView(view)
+        dialog = builder.create()
+        dialog.setCancelable(cancelable)
+    }
+
+    fun setProgressMessage(@StringRes titleRes: Int) {
+        view?.findViewById<TextView>(R.id.text)?.setText(titleRes)
+    }
+
+    fun show() {
+        dialog.show()
+    }
+
+    fun dismiss() {
+        if (dialog.isShowing) {
+            dialog.dismiss()
+        }
+    }
+
+    fun setProgressDialogVisibility(b: Boolean) {
+
+    }
+}
