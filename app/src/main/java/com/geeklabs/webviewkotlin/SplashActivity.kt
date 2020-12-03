f06599d2ad2bc311507f70b7308af103822482d3
Shylendra Madda <shylendramadda@gmail.com>
2020-12-03 22:43:05 +0530

    Add video in splash

--- app/src/main/java/com/geeklabs/webviewkotlin/SplashActivity.kt
+++ app/src/main/java/com/geeklabs/webviewkotlin/SplashActivity.kt
@@ -1,17 +1,37 @@
 package com.geeklabs.webviewkotlin
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        withDelay(2000) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+        // To display image
+        /*withDelay(2000) {
+            navigateToMain()
+        }*/
+        // To display video
+        try {
+            val video: Uri =
+                Uri.parse("android.resource://" + packageName + "/" + R.raw.splash_video)
+            videoView.setVideoURI(video)
+            videoView.setOnCompletionListener { navigateToMain() }
+            videoView.start()
+        } catch (ex: Exception) {
+            withDelay(2000) {
+                navigateToMain()
+            }
         }
+    }
+
+    private fun navigateToMain() {
+        if (isFinishing) return
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
